372065878df6636e1b909265645f3030920df18c
NezaShalom <igishalom672@gmail.com>
2024-08-08 20:50:15 -0400

    reusable component | Props

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
+import PatientForm from "@/components/forms/PatientForm";
+import { Link } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex h-screen max-h-screen">
+{/*       
+      OTP Verfication | PassKey Modal popup */}
+
+
+      {/* Full/ page layout */}
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image 
@@ -12,8 +19,33 @@ export default function Home() {
             alt="patient"
             className="mb-12 h-10 w-fit"
           />
+
+          {/* Patient Fill out form section  */}
+      
+          <PatientForm />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 CarePulse
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
         </div>
       </section>
+{/* 
+      Big image section */}
+
+      <Image 
+        src="/assets/images/hello-the-one.jpg"
+        height={1000}
+        width={1000}
+        alt="patient"
+        /** It takes 50% of the screen */
+        className="side-img max-w-[50%]"
+        />
+
     </div>
   );
 }
